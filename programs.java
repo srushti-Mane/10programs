658837932570358911734277ce66ba9f9b8ccbb2
srushti-Mane <srushtideshmukh2909@gmail.com>
2022-08-16 18:14:25 +0530

    modified:code for sum of numbers given range

--- programs.java
+++ programs.java
@@ -1,9 +1,10 @@
 public class programs {
  public static void main(String[] args) {
-     int i = 10;
+     int s = 5;
+     int r = 10;
      int sum = 0;
-     for (int n = 1; n <= i ;n++)
-       sum += n;
-     System.out.println(sum);
+     for (int u = 1; u <= r ;u++)
+       sum= sum+ u;
+     System.out.println("sum is " + sum);
  }
 }
