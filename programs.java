9adda6b50d8261f61d863276f4bf79e37ff1a456
srushti-Mane <srushtideshmukh2909@gmail.com>
2022-08-16 18:21:49 +0530

    modified:code for greatest of two numbers

--- programs.java
+++ programs.java
@@ -1,10 +1,11 @@
 public class programs {
  public static void main(String[] args) {
-     int s = 5;
-     int r = 10;
-     int sum = 0;
-     for (int u = 1; u <= r ;u++)
-       sum= sum+ u;
-     System.out.println("sum is " + sum);
+     int srushti = 70, poonam = 70;
+     if(srushti == poonam)
+         System.out.println("both equal");
+     else if (srushti > poonam)
+         System.out.println(srushti + " greater");
+     else
+         System.out.println(poonam + " greater");
  }
 }
