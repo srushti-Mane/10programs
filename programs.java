9d87dcf9cf2cca990e28fba60a6d0f9a87beb281
srushti-Mane <srushtideshmukh2909@gmail.com>
2022-08-16 18:02:47 +0530

    modified:code for sum of nth natural number

--- programs.java
+++ programs.java
@@ -1,9 +1,9 @@
 public class programs {
  public static void main(String[] args) {
-     int num = 53;
-     if (num % 2 == 0)
-     {System.out.println("num is even");}
-         else
-     {System.out.println("number is odd");}
+     int i = 5;
+     int sum = 0;
+     for (int n = 1; n <= i ;n++)
+       sum += n;
+     System.out.println(sum);
  }
 }
