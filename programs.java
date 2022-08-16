f761c281ca23d66f27a5252659b2c6847e3e3fb0
srushti-Mane <srushtideshmukh2909@gmail.com>
2022-08-16 17:36:29 +0530

    added:positive or negative number

---
+++ programs.java
@@ -0,0 +1,11 @@
+public class programs {
+ public static void main(String[] args) {
+     int num = 9;
+     if (num > 0)
+     {System.out.println("num is positve");}
+     else if (num < 0)
+     { System.out.println("num is negative");}
+         else
+     {System.out.println("zero");}
+ }
+}
